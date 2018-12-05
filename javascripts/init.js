124b892bb3021070982850edf998721ec5341ba9
azinudinachzab <azinudinachzab@student.uns.ac.id>
2018-12-05 19:49:25 +0700

    index.html

---
+++ javascripts/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    //$('.sidenav').sidenav({edge:'right'});
+    $('.parallax').parallax();
+    $(".button-collapse").sideNav();
+    //$(".dropdown-trigger").dropdown({constrainWidth: false});
+    //$('.slider').slider({height: 500, interval: 5000});
+    //$('.carousel').carousel();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
