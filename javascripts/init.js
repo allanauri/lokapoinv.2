a3746052630527831002c71fb5b285f5df9e8754
Azinudin Achzab <azinudinachzab@student.uns.ac.id>
2018-12-13 22:56:25 +0700

    navbar after login and before login, login/signup form

--- javascripts/init.js
+++ javascripts/init.js
@@ -5,6 +5,7 @@
     $('.parallax').parallax();
     $(".dropdown-trigger").dropdown();
     $('.slider').slider({indicators: false,height: 500});
+    $('.modal').modal();
     //$('.carousel').carousel();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
